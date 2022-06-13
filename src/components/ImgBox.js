d41fa54b61c2cbeebc330f5cff8ace3e62c20c74
Aarya Chopkar <aaryachopkar@gmail.com>
2022-06-13 16:12:54 +0530

    ImgBox

--- src/components/ImgBox.js
+++ src/components/ImgBox.js
@@ -9,8 +9,8 @@ function ImgBox(){
             <h1>Aarya Chopkar</h1>
             <h3>Frontend Developer</h3>
             <div className="btn">
-            <button>Email</button>
-            <button> <i class="fa-brands fa-linkedin"></i> LinkedIn</button>
+                <button> <i class="fa-solid fa-envelope"></i> Email</button>
+                <button> <i class="fa-brands fa-linkedin"></i> LinkedIn</button>
             </div>
         </div>
         </>
