511dc70ffeff533c9da4921a190122f5690dcb66
Aarya Chopkar <aaryachopkar@gmail.com>
2022-06-14 14:42:58 +0530

    Interests Component

---
+++ src/components/Interests.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Interests(){
+    return(
+        <>
+        <div className="interest">
+            <h2>Interests</h2>
+            <p className="text">
+            Photographer. Music Aficionado. Reader. Internet fanatic. Badminton lover. 
+            Travel geek. Coffee fanatic.
+            </p>
+        </div>
+        </>
+    )
+}
+
+export default Interests;
