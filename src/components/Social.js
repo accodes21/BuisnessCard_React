902b4b03f38e5fb492790ac4ae42f7b1345752a3
Aarya Chopkar <aaryachopkar@gmail.com>
2022-06-14 14:43:31 +0530

    Social Component

---
+++ src/components/Social.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Social(){
+    return(
+        <>
+        <div className="social">
+            <ul>
+                <li><a href="https://twitter.com/ChopkarAarya"><i class="fa-brands fa-twitter-square"></i></a></li>
+                <li><a href="https://github.com/accodes21"><i class="fa-brands fa-facebook-square"></i></a></li>
+                <li><a href="https://github.com/accodes21"><i class="fa-brands fa-github-square"></i></a></li>
+            </ul>
+        </div>
+        </>
+    )
+}
+
+export default Social;
